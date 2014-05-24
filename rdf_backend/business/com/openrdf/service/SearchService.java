d693df5bba1ad31ea8c23fc7f136540848b2fe97
Xianbin Xie <a.b@hotmail.com>
2014-05-24 19:38:15 +0800

    udpate dao - action

--- rdf_backend/business/com/openrdf/service/SearchService.java
+++ rdf_backend/business/com/openrdf/service/SearchService.java
@@ -1,14 +1,36 @@
 package com.openrdf.service;
 
+import java.util.List;
+
+import com.openrdf.beans.SearchHistory;
+import com.openrdf.beans.SearchStatistics;
 import com.openrdf.dao.SearchDao;
 
 public class SearchService {
 
 	private SearchDao searchDao;
 	
+	/**
+	 * 获取搜索记录 
+	 * 
+	 * @return
+	 */
+	public List<SearchHistory> getSearchHistoryList(){
+		
+		List<SearchHistory> searchServiceList = searchDao.getSearchHistoryList();
+		return searchServiceList;
+	}
 	
+	/**
+	 * 获取搜索统计 
+	 * 
+	 * @return
+	 */
+	public List<SearchStatistics> searchStatistics() {
+		return searchDao.getSearchStatistisc();
+	}
 	
-	
+	/**                getters and setters                       **/
 	public SearchDao getSearchDao() {
 		return searchDao;
 	}
@@ -16,4 +38,5 @@ public class SearchService {
 	public void setSearchDao(SearchDao searchDao) {
 		this.searchDao = searchDao;
 	}
+
 }
